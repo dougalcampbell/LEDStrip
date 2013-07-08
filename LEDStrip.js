ac217336e89c24884f6900810697eb652fec7f69
Dougal Campbell <dougal@gunters.org>
2013-07-07 22:49:19 -0400

    Added requestAnimationFrame shim. Split out files.

--- LEDStrip.js
+++ LEDStrip.js
@@ -78,6 +78,14 @@ LEDStrip.prototype.latch = function () {
 		}
 }
 
+// Set colors in one big batch. Array of RGB triplets.
+LEDStrip.prototype.send = function(vals) {
+	for (var i = 0; i < this.len; i++) {
+		this.pushRGB(vals[i][0], vals[i][1], vals[i][2]);
+	}
+	this.latch();
+}
+
 /**
  * Individual LED module
  */
@@ -96,6 +104,7 @@ LED.prototype.latch = function () {
 		this.next.latch();
 	}
 }
+
 LED.prototype.datain = function (r, g, b) {
 	if (this.rgb.length) {
 		this.dataout(this.rgb[0], this.rgb[1], this.rgb[2]);
@@ -109,3 +118,166 @@ LED.prototype.dataout = function (r, g, b) {
 	}
 }
 
+
+/****
+ * Do eet!
+ * 
+ * This code currently relies on some global variables, which I don't like.
+ * It's a side-effect of my translation of the original AVR C++ code. I 
+ * need to wrap it up and encapsulate it better, maybe with some IoC for
+ * the connection between my LEDStrip class and this controller code.
+ */
+var strip, animation;
+var length = 48;
+var r = g = b = count = 0;
+var leds = Array(length);
+var chasers = [
+	new Chaser([200, 75, 75], 0, true),
+	new Chaser([75, 200, 75], Math.floor(length * 0.2), false),
+	new Chaser([75, 75, 200], Math.floor(length * 0.4), true),
+	new Chaser([200, 200, 75], Math.floor(length * 0.6), false),
+	new Chaser([220, 75, 175], Math.floor(length * 0.8), true),
+	];
+
+$(document).ready(function() {
+	strip = new LEDStrip(length, $('.ledstrip'), $('.ledlight'));
+	animation = requestAnimationFrame(chase);
+  
+  $('#animselect').change(function(e) {
+  var newanim = $(e.target).val();
+  console.log('change! ' + newanim); 
+  cancelAnimationFrame(animation);
+  switch(newanim) {
+    case 'colorfade': 
+      animation = requestAnimationFrame(ledtick);
+      console.log('ledtick ' + animation);
+      break;
+    case 'chasers':
+      animation = requestAnimationFrame(chase);
+      console.log('chasers ' + animation);
+      break;
+    case 'stop':
+      console.log('stop ' + animation);
+      break;
+  }
+  });
+});
+
+function chase() {
+	animation = requestAnimationFrame(chase);
+	// slow things down. 1 == full speed
+    if ((count++ % 3)) return;
+	clearLeds(leds);
+	for ( var i = 0; i < chasers.length; i++ ) {
+		chasers[i].step(leds);
+	}
+	strip.send(leds);
+}
+
+/**
+ * CHASERS: https://github.com/DannyHavenith/ws2811
+ */
+function clearLeds(buf) {
+	for (i=0; i < length; i++)
+		buf[i] = [0,0,0];
+}
+
+function Chaser(color, position, forward) {
+	this.color = color;
+	this.position = position;
+	this.forward = forward;
+  /* I changed these scaling values from the originals, because they were
+   * too dark to show a good trail on a computer monitor.
+   */
+	this.amplitudes = [256, 200, 175, 150, 125, 100, 80, 60, 50, 40, 30, 20, 15, 10, 6, 1];
+
+	return this;
+}
+
+Chaser.prototype.step = function(buf) {
+	this._step(length);
+	this._draw(buf);
+}
+
+Chaser.prototype._step = function(size) {
+	if (this.forward) {
+		if (++this.position > size) {
+			this.position = size - 1;
+			this.forward = false;
+		}
+	} else {
+		if (!--this.position) {
+			this.forward = true;
+		}
+	}
+}
+
+Chaser.prototype._draw = function(buf) {
+	var step = this.forward ? -1 : 1;
+	var pos = this.position;
+
+	for (var count = 0; count < this.amplitudes.length; ++count) {
+		var value = this._scale(this.color, this.amplitudes[count]);
+		buf[pos] = this._addClipped(buf[pos], value);
+		pos += step;
+
+		if (pos == length) {
+			step = -step;
+			pos = length - 1;
+		}
+	}
+}
+
+Chaser.prototype._scale = function(color, amp) {
+	var r, g, b;
+
+	r = (color[0] * amp) >> 8;
+	g = (color[1] * amp) >> 8;
+	b = (color[2] * amp) >> 8;
+
+	return [r, g, b];
+}
+
+Chaser.prototype._addClipped = function(rgb1, rgb2) {
+	var newrgb = Array(3);
+  // for some reason, we sometimes get undefined values. Error check.
+	if (! Array.isArray(rgb1)) rgb1 = [0,0,0];
+	if (! Array.isArray(rgb2)) rgb2 = [0,0,0];
+
+	newrgb[0] = rgb1[0] + rgb2[0];
+	newrgb[1] = rgb1[1] + rgb2[1];
+	newrgb[2] = rgb1[2] + rgb2[2];
+
+	newrgb[0] = newrgb[0] > 255 ? 255 : newrgb[0];
+	newrgb[1] = newrgb[1] > 255 ? 255 : newrgb[1];
+	newrgb[2] = newrgb[2] > 255 ? 255 : newrgb[2];
+
+	return newrgb;	
+}
+
+// alternate looping color animation
+function ledtick() {
+	animation = requestAnimationFrame(ledtick);
+
+	// slow things down. 1 == full speed
+    if ((count++ % 3)) return;
+
+	for (var i = 0; i < length; i++) {
+		r+=Math.floor(256/length);
+		if (r >= 255) {
+			r = 0;
+			g+=Math.floor(256/length);
+		}
+		if (g >= 255) {
+			g = 0;
+			b+=Math.floor(256/length);
+		}
+		if (b >= 255)  {
+			b = 0;
+		}
+	    leds[i] = [r,g,b];
+  	}
+    strip.send(leds);
+    //console.log('LATCH');
+    //console.log(r + ', ' + g + ', ' + b);
+}
